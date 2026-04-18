bf06afa8e2ee98396e3fae28361841a71a995068
p.phonsing_ <aprilpollo.apl@gmail.com>
2026-04-19 00:36:04 +0700

    fix: adjust class names for consistency and improve layout in KanbanColumn component

--- src/components/kanban/column.tsx
+++ src/components/kanban/column.tsx
@@ -65,7 +65,7 @@ export function KanbanColumn({ column, tasks }: KanbanColumnProps) {
         }}
         className="relative flex h-fit max-h-[calc(100vh-165px)] w-66 shrink-0 flex-col items-start rounded-sm border bg-card/50 opacity-50"
       >
-        <div className="sticky w-full top-0 z-10 flex items-center justify-between p-2">
+        <div className="sticky top-0 z-10 flex w-full items-center justify-between p-2">
           <div className="flex items-center gap-2">
             <Button
               style={{ color: column.color }}
@@ -128,82 +128,91 @@ export function KanbanColumn({ column, tasks }: KanbanColumnProps) {
         ...style,
         // backgroundColor: `${column.color}09`,
       }}
-      className="relative h-fit w-66 shrink-0 items-start rounded-sm border bg-card/50"
+      className="relative w-66 shrink-0 items-start rounded-sm "
     >
-      <div data-slot="kanban-column-header" className="sticky top-0 z-10 flex w-full items-center justify-between p-2">
-        <div className="flex items-center gap-2">
-          <Button
-            {...attributes}
-            {...listeners}
-            variant="ghost"
-            size="icon"
-            className="h-6 w-6 cursor-grab rounded-sm active:cursor-grabbing"
-          >
-            <GripVertical className="h-4 w-4" />
-          </Button>
-          <span
-            className="rounded-[3px] px-1 text-xs font-semibold uppercase"
-            style={{
-              backgroundColor: column.color,
-              color: getContrastColor(column.color || "#A1A1A1"),
+      <div className="bg-card rounded-sm">
+        <div
+          data-slot="kanban-column-header"
+          className="sticky top-0 z-10 flex w-full items-center justify-between p-2"
+        >
+          <div className="flex items-center gap-2">
+            <Button
+              {...attributes}
+              {...listeners}
+              variant="ghost"
+              size="icon"
+              className="h-6 w-6 cursor-grab rounded-sm active:cursor-grabbing"
+            >
+              <GripVertical className="h-4 w-4" />
+            </Button>
+            <span
+              className="rounded-[3px] px-1 text-xs font-semibold uppercase"
+              style={{
+                backgroundColor: column.color,
+                color: getContrastColor(column.color || "#A1A1A1"),
+              }}
+            >
+              {column.name}
+            </span>
+            <Badge
+              className="h-5 min-w-5 rounded-full px-1 font-mono tabular-nums"
+              variant="outline"
+            >
+              {columnPagination[column.id]?.total ?? tasks.length}
+            </Badge>
+          </div>
+          <div className="flex items-center gap-1">
+            <Button
+              variant="ghost"
+              size="icon"
+              className="h-6 w-6 cursor-pointer rounded-full"
+              onClick={() => setAddTaskOpen(!addTaskOpen)}
+            >
+              <Plus className={cn("h-4 w-4", addTaskOpen && "rotate-45")} />
+            </Button>
+            <DropdownMenuColumn
+              column={column}
+              onAddTask={() => setAddTaskOpen(true)}
+            />
+          </div>
+        </div>
+
+        <div ref={scrollContainerRef}>
+          <ScrollArea
+            className="max-h-[calc(100vh-220px)] px-1 *:data-[slot=scroll-area-viewport]:max-h-[calc(100vh-220px)]!"
+            onScrollCapture={(e) => {
+              const target = e.target as HTMLElement
+              const bottom =
+                target.scrollHeight - target.scrollTop - target.clientHeight <
+                50
+              if (bottom) {
+                loadMoreTasks(column.id)
+              }
             }}
           >
-            {column.name}
-          </span>
-          <Badge
-            className="h-5 min-w-5 rounded-full px-1 font-mono tabular-nums"
-            variant="outline"
-          >
-            {columnPagination[column.id]?.total ?? tasks.length}
-          </Badge>
+            <SortableContext
+              items={taskIds}
+              strategy={verticalListSortingStrategy}
+            >
+              <div className="space-y-2 px-1 py-2">
+                {addTaskOpen && (
+                  <AddTask
+                    columnId={column.id}
+                    setAddTaskOpen={setAddTaskOpen}
+                  />
+                )}
+                {tasks.map((task) => (
+                  <KanbanCard key={task.id} task={task} isFaded={addTaskOpen} />
+                ))}
+                {columnPagination[column.id]?.loading && (
+                  <div className="flex justify-center p-2">
+                    <Loader2 className="h-4 w-4 animate-spin text-muted-foreground" />
+                  </div>
+                )}
+              </div>
+            </SortableContext>
+          </ScrollArea>
         </div>
-        <div className="flex items-center gap-1">
-          <Button
-            variant="ghost"
-            size="icon"
-            className="h-6 w-6 cursor-pointer rounded-full"
-            onClick={() => setAddTaskOpen(!addTaskOpen)}
-          >
-            <Plus className={cn("h-4 w-4", addTaskOpen && "rotate-45")} />
-          </Button>
-          <DropdownMenuColumn
-            column={column}
-            onAddTask={() => setAddTaskOpen(true)}
-          />
-        </div>
-      </div>
-
-      <div ref={scrollContainerRef}>
-        <ScrollArea
-          className="max-h-[calc(100vh-220px)] px-1 *:data-[slot=scroll-area-viewport]:max-h-[calc(100vh-220px)]!"
-          onScrollCapture={(e) => {
-            const target = e.target as HTMLElement
-            const bottom =
-              target.scrollHeight - target.scrollTop - target.clientHeight < 50
-            if (bottom) {
-              loadMoreTasks(column.id)
-            }
-          }}
-        >
-          <SortableContext
-            items={taskIds}
-            strategy={verticalListSortingStrategy}
-          >
-            <div className="space-y-2 px-1 py-2">
-              {addTaskOpen && (
-                <AddTask columnId={column.id} setAddTaskOpen={setAddTaskOpen} />
-              )}
-              {tasks.map((task) => (
-                <KanbanCard key={task.id} task={task} isFaded={addTaskOpen} />
-              ))}
-              {columnPagination[column.id]?.loading && (
-                <div className="flex justify-center p-2">
-                  <Loader2 className="h-4 w-4 animate-spin text-muted-foreground" />
-                </div>
-              )}
-            </div>
-          </SortableContext>
-        </ScrollArea>
       </div>
     </div>
   )
